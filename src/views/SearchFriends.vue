8703c111ab2cbbf6572ea64a5b4391440efee15e
Polapob <71330294+Polapob@users.noreply.github.com>
2022-08-08 14:53:25 +0700

    remove border black

--- src/views/SearchFriends.vue
+++ src/views/SearchFriends.vue
@@ -22,11 +22,7 @@ const onPrevPage = () => {
     <div class="font-bold text-[20px] my-2">You can search your friends</div>
     <input class="my-5 rounded-md px-4 py-1 w-[300px] focus:outline-none" />
     <div class="flex flex-col justify-center items-start w-[700px] gap-y-4 my-3">
-      <div
-        v-for="friend in filterDataFromPagination(selectPage, 15, numberOfFriends, friends.data)"
-        v-bind:key="friend.id"
-        class="w-full border-2 border-black"
-      >
+      <div v-for="friend in filterDataFromPagination(selectPage, 15, numberOfFriends, friends.data)" v-bind:key="friend.id" class="w-full">
         <div class="flex flex-row justify-between items-center w-full border-2 px-8 py-4 rounded-xl bg-white">
           <div>เลขประจำตัว: {{ friend.id }}</div>
           <div>ชื่อจริง: {{ friend.fullName }}</div>
