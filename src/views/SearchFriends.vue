333bc3db0b34e03269dedc21502d1dea5b95d85b
Polapob <71330294+Polapob@users.noreply.github.com>
2022-08-15 23:00:37 +0700

    remove input text display

--- src/views/SearchFriends.vue
+++ src/views/SearchFriends.vue
@@ -20,7 +20,6 @@ const scrollToTop = () => {
     <div class="font-bold text-2xl">Show List of your friends</div>
     <div class="font-bold text-[20px] my-2">You can search your friends</div>
     <input class="my-5 rounded-md px-4 py-1 w-[300px] focus:outline-none" v-model="input" />
-    {{ input }}
     <div class="flex flex-col justify-center items-start w-[700px] gap-y-4 my-3">
       <div v-for="friend in friends.data" v-bind:key="friend.id" class="w-full">
         <div class="flex flex-row justify-between items-center w-full border-2 px-8 py-4 rounded-xl bg-white">
