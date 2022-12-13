36160cc7cc4a2bef7a35b43ab74a0a21f6e5eba0
Polapob <71330294+Polapob@users.noreply.github.com>
2022-12-13 12:37:37 +0700

    change user information design

--- src/views/ViewFriend.vue
+++ src/views/ViewFriend.vue
@@ -17,15 +17,34 @@ onMounted(async () => {
 });
 </script>
 <template>
-  <div class="flex flex-col justify-center items-center">
-    <div class="font-bold text-[20px]">ข้อมูลที่ได้จากการค้นหาข้อมูล</div>
-    <div class="flex flex-col justify-center items-center" v-if="friendInfo.tu_id">
-      <div>Triamudom ID = {{ friendInfo.tu_id && friendInfo.tu_id.split("-").slice(-1).toString() }}</div>
-      <div>เตรียมอุดมรุ่นที่ {{ friendInfo.tu_id && friendInfo.tu_id.split("-")[0].toString() }}</div>
-      <div v-if="friendInfo.is_member === '1'">เป็นสามัญสมาชิก</div>
-      <div v-else>ไม่เป็นสามัญสมาชิก</div>
-      <div>ชื่อ: {{ friendInfo.first_name }}</div>
-      <div>นามสกุล: {{ friendInfo.last_name }}</div>
+  <div class="flex flex-col justify-start items-center">
+    <div class="font-bold text-[28px] mb-4">ข้อมูลที่ได้จากการค้นหาข้อมูล</div>
+    <div class="flex flex-col justify-center items-start w-3/4" v-if="friendInfo.first_name">
+      <div class="font-bold text-[24px]">
+        {{ friendInfo.prefix }} {{ friendInfo.first_name }} {{ friendInfo.last_name }} (รุ่นที่ {{ friendInfo.generation_id }})
+      </div>
+
+      <div class="text-[22px] my-2">ข้อมูลทั่วไป</div>
+      <div class="flex flex-row justify-start items-center text-[18px] w-full p-2 bg-gray-100">
+        <div class="w-[200px] font-bold">ชื่อ-สกุล ภาษาอังกฤษ</div>
+        <div v-if="friendInfo.first_name_eng">{{ friendInfo.first_name_eng }} {{ friendInfo.last_name_eng }}</div>
+        <div v-else>ไม่ระบุ</div>
+      </div>
+      <div class="flex flex-row justify-start items-center text-[18px] w-full p-2">
+        <div class="w-[200px] font-bold">สถานะ</div>
+        <div v-if="friendInfo.is_member === '1'">เป็นสามัญสมาชิค</div>
+        <div v-else>ไม่เป็นสามัญสมาชิค</div>
+      </div>
+      <div class="flex flex-row justify-start items-center text-[18px] w-full p-2 bg-gray-100">
+        <div class="w-[200px] font-bold">รหัสประจำตัวนักเรียน</div>
+        <div>{{ friendInfo.tu_id.split("-").splice(-1).toString() }}</div>
+      </div>
+      <div class="flex flex-row justify-start items-center text-[18px] w-full p-2">
+        <div class="w-[200px] font-bold">เพศ</div>
+        <div v-if="friendInfo.sex === 'f'">หญิง</div>
+        <div v-else-if="friendInfo.sex === 'm'">ชาย</div>
+        <div v-else>ไม่ระบุ</div>
+      </div>
     </div>
   </div>
 </template>
