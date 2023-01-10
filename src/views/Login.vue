654113b5ee7a1fe66613876a4d8eaf172b44a5ed
Polapob <71330294+Polapob@users.noreply.github.com>
2023-01-10 22:25:32 +0700

    update responsive design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,7 +17,7 @@ if (isUserLogin) {
 
 <template>
   <div class="p-8 flex flex-col justify-center items-center gap-y-6 min-h-[90vh] bg-[url('/background/loginBackground.png')]">
-    <div class="flex flex-col justify-center items-center w-[500px] gap-y-8" @keyup.enter="handleEnterpress">
+    <div class="flex flex-col justify-center items-center lg:w-[500px] w-full gap-y-8" @keyup.enter="handleEnterpress">
       <div class="text-3xl font-bold w-full text-start">Log In</div>
       <div class="flex flex-col justify-start items-center w-full gap-y-4">
         <div class="flex flex-col justify-center items-start w-full">
@@ -37,7 +37,7 @@ if (isUserLogin) {
         </div>
       </div>
 
-      <div class="relative min-w-[550px] flex justify-center items-center h-16">
+      <div class="relative lg:w-[550px] w-full flex justify-center items-center h-16">
         <button
           ref="buttonRef"
           :disabled="isLoading"
--- src/views/SearchFriends.vue
+++ src/views/SearchFriends.vue
@@ -21,37 +21,32 @@ const scrollToTop = () => {
 };
 </script>
 <template>
-  <div class="flex flex-col justify-start items-center py-8 px-32 min-h-screen relative">
+  <div class="flex flex-col justify-start items-center py-8 lg:px-32 px-3 min-h-screen relative">
     <div class="hidden">{{ selectPage }}</div>
-    <div class="font-bold text-2xl">ค้นหาเพื่อนจากชื่อ หรือ นามสกุล</div>
-    <div class="flex flex-row justify-start items-center border border-[#DFDFDF] w-[80%] mx-6 px-6 gap-x-4 bg-[#F3F3F3] my-5 rounded-md py-3">
+    <div class="font-bold lg:text-2xl text-[20px] leading-7">ค้นหาเพื่อนจากชื่อ หรือ นามสกุล</div>
+    <div class="flex flex-row justify-start items-center border border-[#DFDFDF] sm:w-[80%] w-[90%] mx-6 px-6 gap-x-4 bg-[#F3F3F3] my-5 rounded-md py-3">
       <input class="focus:outline-none w-full bg-inherit" placeholder="Search" v-model="input" />
-      <button class="bg-[#F4B3BB] px-10 py-2 rounded-md hover:bg-opacity-80 transition-[background-color]">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-      </button>
     </div>
-    <div class="flex flex-col justify-center items-center gap-y-4 my-3 w-[80%]">
+    <div class="flex flex-col justify-center items-center gap-y-4 my-3 lg:w-[80%] w-full px-3">
       <div class="flex flex-row justify-between items-center w-full border-b border-b-[#DFDFDF] py-4 my-2 text-[rgba(0,0,0,0.4)]">
-        <div class="w-[150px]">NAME</div>
+        <div class="lg:w-[150px] w-[135px]">NAME</div>
         <div class="">ID</div>
         <div />
       </div>
       <div v-if="isEmptyFriend" class="w-full flex flex-col justify-center items-center">
-        <div class="text-[20px] font-bold">Sorry we didn't find any friends in the database.</div>
+        <div class="sm:text-[20px] text-[16px] font-bold">ไม่พบเพื่อนที่มีชื่อตรงกับที่คุณกรอก</div>
       </div>
       <div v-for="friend in friends" v-bind:key="friend.id" class="w-full">
         <div class="flex justify-between items-center w-full py-4 rounded-xl">
-          <div class="w-[250px]">{{ friend.fullName }}</div>
+          <div class="lg:w-[250px] w-[200px] sm:text-[16px] text-[14px]">{{ friend.fullName }}</div>
           <div class="">{{ friend.generationId }}</div>
           <router-link :to="{ path: `view-friend/${friend.id}` }">
-            <button class="rounded-xl px-8 py-2 bg-blue-500 text-white font-semibold hover:bg-opacity-80 transition-colors duration-200">View</button>
+            <button class="rounded-xl lg:px-8 px-4 py-2 bg-blue-500 text-white font-semibold hover:bg-opacity-80 transition-colors duration-200 sm:text-[16px] text-[14px]">View</button>
           </router-link>
         </div>
       </div>
     </div>
-    <button class="absolute bottom-[32px] right-[64px] w-12 h-12 rounded-full bg-blue-500 flex justify-center items-center" @click="scrollToTop">
+    <button class="fixed bottom-[32px] sm:right-[64px] right-5 w-12 h-12 rounded-full bg-blue-500 flex justify-center items-center" @click="scrollToTop">
       <font-awesome-icon icon="fa-solid fa-angles-up" class="text-white text-[20px]" />
     </button>
   </div>
