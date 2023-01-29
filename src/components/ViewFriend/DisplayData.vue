93adf7dc465e03fed0dfc89e79e8bf3d403bdc7b
Polapob <71330294+Polapob@users.noreply.github.com>
2023-01-29 17:23:37 +0700

    update view friend logic

--- src/components/ViewFriend/DisplayData.vue
+++ src/components/ViewFriend/DisplayData.vue
@@ -3,12 +3,24 @@ import { defineProps } from "vue";
 defineProps<{ title: string; data: [string, any][] }>();
 </script>
 <template>
-  <div>
-    <div class="font-bold text-[20px] leading-[30px]">{{ title }}</div>
-    <div v-for="([dataTitle, dataValue], index) in data" :key="index">
-      <div class="flex flex-row justify-start items-center">
-        <div>{{ dataTitle }}</div>
-        <div>{{ dataValue }}</div>
+  <div class="w-full mb-4">
+    <div class="font-bold md:text-20 sm:text-18 text-16 mb-3">{{ title }}</div>
+    <div
+      class=""
+      v-for="([dataTitle, dataValue], index) in data"
+      :key="dataTitle">
+      <div
+        class="flex flex-row justify-start gap-x-2 items-center p-2"
+        :class="{
+          'bg-gray-100': index % 2 === 0,
+          'bg-white': index % 2 === 1,
+        }">
+        <div class="min-w-[150px] md:text-18 sm:text-16 text-14 font-semibold">
+          {{ dataTitle }}
+        </div>
+        <div class="md:text-18 sm:text-16 text-14 break-all">
+          {{ dataValue }}
+        </div>
       </div>
     </div>
   </div>
--- src/views/ViewFriend.vue
+++ src/views/ViewFriend.vue
@@ -15,34 +15,52 @@ onMounted(async () => {
   const friendData = await friendService.viewFriend(studentId);
   if (friendData) {
     friendInfo.value = friendDataAdapter(friendData.data.result);
-    console.log(friendDataAdapter(friendData.data.result));
   }
 });
 </script>
 <template>
-  <div class="flex flex-col justify-start items-center">
+  <div class="flex flex-col justify-start items-center pb-10">
     <div class="flex justify-center items-center relative w-full">
       <button
         @click="$router.go(-1)"
-        class="flex-1 bg-red-500 text-[16px] leading-6 text-white py-2 px-4 rounded-lg transition-colors hover:bg-opacity-80 absolute left-0">
+        class="flex-1 bg-red-500 sm:text-16 text-14 text-white py-2 sm:px-4 px-4 rounded-lg transition-colors hover:bg-opacity-80 absolute left-0">
         Back
       </button>
-      <div class="font-bold text-[28px] h-full">
+      <div class="font-bold md:text-28 sm:text-24 text-20 h-full">
         ข้อมูลที่ได้จากการค้นหาข้อมูล
       </div>
     </div>
+
     <div
-      class="flex flex-col justify-center items-start w-[500px]"
+      class="flex flex-col justify-center items-start xl:w-[1000px] w-full gap-y-3 px-3"
       v-if="friendInfo.isShowGeneral">
-      <div>
-        <div class="font-bold text-[24px] leading-9">
-          {{ friendInfo.availableData.prefix }}
-          {{ friendInfo.availableData.first_name }}
-          {{ friendInfo.availableData.last_name }}
-          (รุ่น {{ friendInfo.availableData.generation_id }})
-        </div>
+      <div
+        class="font-bold md:text-24 md:leading-9 sm:text-20 sm:leading-[30px] my-4">
+        {{ friendInfo.availableData.prefix }}
+        {{ friendInfo.availableData.first_name }}
+        {{ friendInfo.availableData.last_name }}
+        (รุ่น {{ friendInfo.availableData.generation_id }})
       </div>
-      <DisplayData title="ข้อมูลทั่วไป" :data="friendInfo.generalInfo" />
+      <DisplayData
+        v-if="friendInfo.isShowGeneral"
+        title="ข้อมูลทั่วไป"
+        :data="friendInfo.generalInfo" />
+      <DisplayData
+        v-if="friendInfo.isShowContact"
+        title="ข้อมูลการติดต่อ"
+        :data="friendInfo.contactInfo" />
+      <DisplayData
+        v-if="friendInfo.isShowEducation"
+        title="ข้อมูลการศึกษา"
+        :data="friendInfo.educationInfo" />
+      <DisplayData
+        v-if="friendInfo.isShowAddress"
+        title="ข้อมูลที่อยู่"
+        :data="friendInfo.addressInfo" />
+      <DisplayData
+        v-if="friendInfo.isShowOffice"
+        title="ข้อมูลที่ทำงาน"
+        :data="friendInfo.officeInfo" />
     </div>
   </div>
 </template>
