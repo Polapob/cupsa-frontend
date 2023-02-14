a3e1a5e4fd98bd1215f08eb9790a34c624a6b68b
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-14 19:13:09 +0700

    remove image_url and image_2_url from table

--- src/utils/ViewFriend/filterDefineValue.ts
+++ src/utils/ViewFriend/filterDefineValue.ts
@@ -1,10 +1,13 @@
 const filterDefineValue = <T extends Object>(data: T) => {
   const filterArray = Object.entries(data).reduce((prev, curr) => {
     const [key, value] = curr;
+    if (key === "image_url" || key === "image_2_url") {
+      return prev;
+    }
     if (value) {
       return [...prev, curr];
     }
-    return [...prev];
+    return prev;
   }, [] as [string, any][]);
   return filterArray;
 };
